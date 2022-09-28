3588732af921bd9a38baa823bd8e03d8efcb3944
mathew <kramer.mathew.a@gmail.com>
2022-09-28 16:19:48 -0400

    Add styling to vocab page

--- kaki-frontend/src/App.js
+++ kaki-frontend/src/App.js
@@ -74,13 +74,31 @@ const WordList = () => {
   if (error) return <pre>{error.message}</pre>;
 
   return (
-    <table style={{width: 50 + '%'}}><tbody>
+    <section className="wordList">
       {data.words.map((word) => (
-        <tr key={word.id}>
-          <td><b>{word.tango}</b></td><td>{word.yomi}</td><td>{word.pitch}</td><td>学習済み <UpdateWord word={word}/></td><td><DeleteWord word={word}/></td>
-        </tr>
+        <article className="wordCard" key={word.id} data-learned={word.learned}>
+          <h2 className="tango">{word.tango}</h2>
+          <p style={{"--pitch": word.pitch}}>
+            {word.yomi.split('').map((mora, i) => {
+              if(i == 0) {
+                if(word.pitch == 1)
+                  return <span className="mora" key={mora} data-pitch="peak">{mora}</span>
+              }
+              else {
+                if (word.pitch == 0)
+                  return <span className="mora" key={mora} data-pitch="high">{mora}</span>
+                if(i < word.pitch - 1)
+                  return <span data-pitch="high">{mora}</span>
+                else if (i == word.pitch - 1)
+                  return <span className="mora" key={mora} data-pitch="peak">{mora}</span>
+              }
+              return <span className="mora" key={mora} data-pitch="low">{mora}</span>
+            })}
+          <span className="sr-only"> - Pitch accent: {word.pitch}</span></p>
+          <UpdateWord word={word}/><DeleteWord word={word}/>
+        </article>
       ))}
-    </tbody></table>
+    </section>
   );
 };
 
@@ -88,11 +106,12 @@ const DeleteWord = (props) => {
 
   const [deleteWord] = useMutation(DELETE_WORD);
   
-  return(<button onClick={e => {
+  return(<button className="deleteWord" onClick={e => {
     e.preventDefault();
     let id = parseInt(props.word.id);
     deleteWord({variables: {id: id}, refetchQueries: [ { query: VOCAB_QUERY}]})
   }}>
+    <svg width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10 5h4a2 2 0 1 0-4 0ZM8.5 5a3.5 3.5 0 1 1 7 0h5.75a.75.75 0 0 1 0 1.5h-1.32l-1.17 12.111A3.75 3.75 0 0 1 15.026 22H8.974a3.75 3.75 0 0 1-3.733-3.389L4.07 6.5H2.75a.75.75 0 0 1 0-1.5H8.5Zm2 4.75a.75.75 0 0 0-1.5 0v7.5a.75.75 0 0 0 1.5 0v-7.5ZM14.25 9a.75.75 0 0 0-.75.75v7.5a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75Z"></path></svg>
     削除
     </button>
   );
@@ -103,11 +122,11 @@ const UpdateWord = (props) => {
   const [updateWord] = useMutation(UPDATE_WORD);
   
   return(
-  <input type="checkbox" onChange={e => {
+  <label className="learned"><div className="sr-only">学習済み<input type="checkbox" onChange={e => {
     e.preventDefault();
     let id = parseInt(props.word.id);
     updateWord({variables: {id: id, tango: props.word.tango, yomi: props.word.yomi, pitch: props.word.pitch, learned: !props.word.learned}, refetchQueries: [ { query: VOCAB_QUERY}]})
-  }} checked={props.word.learned} ></input>
+  }} checked={props.word.learned} ></input></div></label>
   );
 }
 
@@ -169,9 +188,5 @@ const CreateWord = (tango, yomi, pitch) => {
      </form>
    </div>
   );}
-
-const DeleteItem = (item) => {
-  
-};
 
 export default App;
