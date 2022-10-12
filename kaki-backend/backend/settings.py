a3fea5747254ec5093096de8c9312cd39d04de21
mathew <kramer.mathew.a@gmail.com>
2022-10-12 12:12:57 -0400

    Change back staticfiel_dirs

--- kaki-backend/backend/settings.py
+++ kaki-backend/backend/settings.py
@@ -125,7 +125,7 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
