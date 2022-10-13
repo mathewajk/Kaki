2e5b5bfafae2f183dba6ae6f6109fcc870230889
mathew <kramer.mathew.a@gmail.com>
2022-10-13 17:24:08 -0400

    Remove staticfiles (Again)

--- kaki-backend/backend/settings.py
+++ kaki-backend/backend/settings.py
@@ -130,8 +130,7 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
