1aa8a1a1f456b8ca49efbbed3ce218a4d45c87ca
mathew <kramer.mathew.a@gmail.com>
2022-10-13 18:01:57 -0400

    REquire authentication for GraphQL interface

--- kaki-backend/backend/urls.py
+++ kaki-backend/backend/urls.py
@@ -27,5 +27,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('kaki.urls'))
+    path('', include('kaki.urls')),
+    path('', admin.site.urls)
 ]
--- kaki-backend/kaki/urls.py
+++ kaki-backend/kaki/urls.py
@@ -3,7 +3,8 @@ from kaki.schema import schema
 
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)))
+    path('graphql', csrf_exempt(login_required(GraphQLView.as_view(graphiql=True, schema=schema))))
 ]
