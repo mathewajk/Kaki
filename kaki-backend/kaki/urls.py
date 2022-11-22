6152cd38d5bf1889477f5fd1053861e7c44c8dbb
Mathew A. K <a.kramer103@gmail.com>
2022-11-22 15:49:49 -0500

    Protext graphiql view

--- kaki-backend/kaki/urls.py
+++ kaki-backend/kaki/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from kaki.schema import schema
+from kaki.views import PrivateGraphQLView
 
 from .views import GoogleLoginView
 
@@ -8,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphiql/', PrivateGraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=False, schema=schema))),
     path("google/", GoogleLoginView.as_view(), name = "google")
 ]
--- kaki-backend/kaki/views.py
+++ kaki-backend/kaki/views.py
@@ -10,6 +10,9 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 
 from django.conf import settings
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from graphene_django.views import GraphQLView
+
 from .serializers import VocabItemSerializer
 from .models import VocabItem
 
@@ -24,3 +27,7 @@ class GoogleLoginView(SocialLoginView):
   adapter_class = GoogleOAuth2Adapter
   callback_url = "http://localhost:3000"  # frontend application url
   client_class = OAuth2Client
+
+
+class PrivateGraphQLView(LoginRequiredMixin, GraphQLView):
+    pass
